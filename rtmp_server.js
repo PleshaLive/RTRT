7e1c551bb619a845d5ce607e7d9e426cf2a9f56b
Plesh4kov <plesh4kov@gmail.com>
2025-09-17 10:59:01 +0300

    Add trans config for HLS generation

--- rtmp_server.js
+++ rtmp_server.js
@@ -12,6 +12,18 @@ const config = {
     port: 8001,
     mediaroot: './media',
     allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: process.env.FFMPEG_PATH || 'ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        mp4: true,
+        mp4Flags: '[movflags=faststart]'
+      }
+    ]
   }
 };
 
